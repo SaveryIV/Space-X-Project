7870b43264fd2bcc72cdfea5170d416faa567075
Agustin Soria <soriaagustin8@gmail.com>
2023-09-04 19:50:12 -0300

    Adding Missions component with Slice

---
+++ src/components/Missions.js
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const Missions = () => {
+  return (
+    <>
+      <h1>Hello from Missions!</h1>
+    </>
+  );
+};
+
+export default Missions;
