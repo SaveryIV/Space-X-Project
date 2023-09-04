627bc815203e953e3242dc2b264bf705fb62242d
Agustin Soria <soriaagustin8@gmail.com>
2023-09-04 11:02:39 -0300

    fixing some linter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Hello!</p>
       </header>
     </div>
   );
