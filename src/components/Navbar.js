68e98d886e4127959f5b34a694033ce8321a3e07
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-09-05 12:25:36 +0300

    Added the navbar and styling to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from '../logo/space-logo.png';
+import '../App.css';
 
 const Navbar = () => (
   <header>
-    <div className="title">
-      <div className="container-title">
-        <h1 className="store">Space X</h1>
-        <ul>
-          <li>
-            <NavLink to="/" className="current">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/rockets" className="current">Rockets</NavLink>
-          </li>
-          <li>
-            <NavLink to="/missions" className="current">Missions</NavLink>
-          </li>
-        </ul>
+    <div className="container-title">
+      <div className="container-logo">
+        <img className="logo" src={Logo} alt="spaceX-logo" />
+        <h1 className="store">Space Travelers Hub</h1>
       </div>
+      <ul>
+        <li>
+          <NavLink to="/rockets" className="current">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink to="/missions" className="current">Missions</NavLink>
+        </li>
+        <li>
+          <NavLink to="/" className="current last">My Profile</NavLink>
+        </li>
+      </ul>
     </div>
   </header>
 );
